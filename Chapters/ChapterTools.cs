4871d0fd92db91073c59ae7e57b49780c2bb4215
BrainSoup <soulkeep1989@gmail.com>
2021-08-25 05:58:11 +0300

    Added tools class with colored text output

---
+++ Chapters/ChapterTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Book_Rickhter.Chapters
+{
+    static class ChapterTools
+    {
+        public static void PrintHeader(string header)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(header);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
